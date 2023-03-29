b2e9a4c298f10a753fe839124aef935afde67143
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-29 14:09:52 +0100

    Exercise 4
    Add stop timer

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -11,10 +11,12 @@ namespace Tempo
             Stopwatch crono1 = new Stopwatch();
             crono1.Start();
             Thread.Sleep(500);
+            crono1.Stop();
 
             Stopwatch crono2 = new Stopwatch();
             crono2.Start();
             Thread.Sleep(300);
+            crono2.Stop();
         }
     }
 }
