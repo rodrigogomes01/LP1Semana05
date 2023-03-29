157b641c813e4fe4b90e9a9fadc768624e79e39b
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-29 13:48:13 +0100

    Exercise 4
    Add crono 1 and crono 2

---
+++ Tempo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+        }
+    }
+}
