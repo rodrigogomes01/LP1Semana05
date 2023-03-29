a16fdc39aa6b46b4e327327b5ddcdfbd80d81f64
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-29 14:21:10 +0100

    Exercise 4
    Print the elapsed time

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -12,11 +12,13 @@ namespace Tempo
             crono1.Start();
             Thread.Sleep(500);
             crono1.Stop();
+            Console.WriteLine("crono1: {0:F2}", crono1.Elapsed.TotalSeconds);
 
             Stopwatch crono2 = new Stopwatch();
             crono2.Start();
             Thread.Sleep(300);
             crono2.Stop();
+            Console.WriteLine("crono2: {0:F2}", crono1.Elapsed.TotalSeconds);
         }
     }
 }
