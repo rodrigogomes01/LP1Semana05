b5d0ac6f6e84109773ad6b8220849bf66fc0f36a
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-03-31 13:37:33 +0100

    Exercise 9
    Convert to lower case

---
+++ LowerUnion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LowerUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+        {
+            args[i] = args[i].ToLower();
+        }
+        
+        Console.WriteLine(string.Join(", ", args));
+        }
+    }
+}
